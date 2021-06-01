f346363891d7950388052cd40a7020801c63ef69
Rus <valuiskiuruslan@gmail.com>
2021-06-01 18:37:58 +0300

    Created model relationships.

--- app/Models/ChatMessage.php
+++ app/Models/ChatMessage.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class ChatMessage extends Model
 {
     use HasFactory;
+
+    public function room() {
+        $this->hasOne(ChatRoom::class, 'id', 'chat_room_id');
+    }
+
+    public function user() {
+        $this->hasOne(User::class, 'id', 'user_id');
+    }
 }
