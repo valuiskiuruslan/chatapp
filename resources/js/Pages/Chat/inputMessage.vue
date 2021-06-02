d4a44e5e4338c8a6bf0cd0d267e560f89a00fdde
Rus <valuiskiuruslan@gmail.com>
2021-06-02 14:06:01 +0300

    Created input message to be add new messages

--- resources/js/Pages/Chat/inputMessage.vue
+++ resources/js/Pages/Chat/inputMessage.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>Input message</div>
+    <div class="relative h-10 m-1">
+        <div class="grid grid-cols-6 border-solid border-1 border-light-gray-500">
+            <input
+                type="text"
+                v-model="message"
+                @keyup="sendMessage()"
+                placeholder="Say something..."
+                class="col-span-5 outline-none p-1"
+            />
+        </div>
+    </div>
 </template>
 
 <script>
