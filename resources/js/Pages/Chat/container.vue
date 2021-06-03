3c22fd664e29433b1eabfd593b31e1c92d346449
Rus <valuiskiuruslan@gmail.com>
2021-06-03 15:29:40 +0300

    Connect to getMessages event

--- resources/js/Pages/Chat/container.vue
+++ resources/js/Pages/Chat/container.vue
@@ -47,7 +47,22 @@
                 messages: [],
             };
         },
+        watch: {
+            currentRoom() {
+                this.connect();
+            }
+        },
         methods: {
+            connect() {
+                if (this.currentRoom.id) {
+                    let vm = this;
+                    this.getMessages();
+                    window.Echo.private("chat." + this.currentRoom.id)
+                        .listen('.message.new', e => {
+                            vm.getMessages();
+                        })
+                }
+            },
             getRooms() {
                 axios.get('/chat/rooms')
                     .then(response => {
@@ -60,7 +75,6 @@
             },
             setRoom(room) {
                 this.currentRoom = room;
-                this.getMessages();
             },
             getMessages() {
                 axios.get('/chat/room/' + this.currentRoom.id + '/messages')
